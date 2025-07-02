5560bd72957e3f5ec22feb997392668a3c922a26
Fernando Navas <navasfernando75@gmail.com>
2025-07-01 20:34:47 -0500

    Here we go again 😞
    ayudaaaa

--- backend-fisio/routes/historiaCompletaRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/historiaCompletaRoutes.js
-const express = require('express');
-const router = express.Router();
-const { crearHistoriaCompleta } = require('../controllers/historiaCompletaController');
-
-router.post('/historia-completa', crearHistoriaCompleta);
-
-module.exports = router;
