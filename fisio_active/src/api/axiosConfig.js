d3d424b4d538be5349627553235dc3a87c5d2ed3
Fernando Navas <navasfernando75@gmail.com>
2025-07-09 12:59:49 -0400

    update .env front

--- fisio_active/src/api/axiosConfig.js
+++ fisio_active/src/api/axiosConfig.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
 });
 
 api.interceptors.request.use((config) => {
